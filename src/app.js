ccc04ef183b610277ed10097ec6b41eaf98409ac
Jose Luis Vela <jlvela94@gmail.com>
2025-05-23 19:20:14 +0000

    logica de crear carta random cada refresco implementada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+const suits = ["♣", "♦", "♥", "♠"]
+const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+
+let randomCardSuit = getRandomNumber(suits)
+let randomCardNumber = getRandomNumber(numbers)
+console.log(randomCardNumber, randomCardSuit)
+
+let topSuit = document.querySelector("#topSuit")
+let bottomSuit = document.querySelector("#bottomSuit")
+let cardNumber = document.querySelector("#cardNumber")
+
+if (randomCardSuit == "♦" || randomCardSuit == "♥") {
+  topSuit.className += " text-danger"
+  bottomSuit.className += " text-danger"
+}
+topSuit.innerHTML = `<p>${randomCardSuit}</p>`
+bottomSuit.innerHTML = `<p>${randomCardSuit}</p>`
+cardNumber.innerHTML = `<p>${randomCardNumber}</p>`
 };
+function getRandomNumber(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
