41cbe5e94d209c4a0bdb850fb46404584e81db4b
Jose Luis Vela <jlvela94@gmail.com>
2025-05-26 18:36:33 +0000

    actualisacion

--- src/app.js
+++ src/app.js
@@ -5,27 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = generateCard()
   //write your code here
-const suits = ["♣", "♦", "♥", "♠"]
-const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-
-let randomCardSuit = getRandomNumber(suits)
-let randomCardNumber = getRandomNumber(numbers)
-console.log(randomCardNumber, randomCardSuit)
-
-let topSuit = document.querySelector("#topSuit")
-let bottomSuit = document.querySelector("#bottomSuit")
-let cardNumber = document.querySelector("#cardNumber")
-
-if (randomCardSuit == "♦" || randomCardSuit == "♥") {
-  topSuit.className += " text-danger"
-  bottomSuit.className += " text-danger"
-}
-topSuit.innerHTML = `<p>${randomCardSuit}</p>`
-bottomSuit.innerHTML = `<p>${randomCardSuit}</p>`
-cardNumber.innerHTML = `<p>${randomCardNumber}</p>`
-};
+function generateCard() {
+  
+  const suits = ["♣", "♦", "♥", "♠"]
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+  
+  let randomCardSuit = getRandomNumber(suits)
+  let randomCardNumber = getRandomNumber(numbers)
+  console.log(randomCardNumber, randomCardSuit)
+  
+  let topSuit = document.querySelector("#topSuit")
+  let bottomSuit = document.querySelector("#bottomSuit")
+  let cardNumber = document.querySelector("#cardNumber")
+  
+  if (randomCardSuit == "♦" || randomCardSuit == "♥") {
+    topSuit.className += " text-danger"
+    bottomSuit.className += " text-danger"
+  }
+  topSuit.innerHTML = `<p>${randomCardSuit}</p>`
+  bottomSuit.innerHTML = `<p>${randomCardSuit}</p>`
+  cardNumber.innerHTML = `<p>${randomCardNumber}</p>`
+  };
 function getRandomNumber(array) {
   return array[Math.floor(Math.random() * array.length)]
 }
